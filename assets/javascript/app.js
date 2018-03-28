f5e99ff39f9a1c0c773f652bec322bad99043276
Matheus Ribeiro <bankrollszn@gmail.com>
2018-03-28 15:37:59 -0400

    Fixed new buttons bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,95 +1,95 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // original array 
-var animals = ["cat", "dog", "rabbit"];
+    var animals = ["cat", "dog", "rabbit"];
 
-// creates a button for every item within the animals array
-function renderButtons() {
-    $("#buttonsHolder").empty();
+    // creates a button for every item within the animals array
+    function renderButtons() {
+        $("#buttonsHolder").empty();
 
-for (let i = 0; i < animals.length; i++) {
-    
-    var animalName = animals[i];
+        for (let i = 0; i < animals.length; i++) {
 
-    var appendAnimal = $("<button>");
+            var animalName = animals[i];
 
-    appendAnimal.addClass("animal");
+            var newButton = $("<button>");
 
-    appendAnimal.attr("data-animal", animalName);
+            newButton.addClass("animal");
+            newButton.on("click", getGifs);
+            newButton.attr("data-animal", animalName);
 
-    appendAnimal.text(animalName);
+            newButton.text(animalName);
 
-    $("#buttonsHolder").append(appendAnimal);
-
-    
-  }
-}
-//adds new buttons to the page
-$("body").on("click", "#submitAnimal", function(event) {
-  event.preventDefault();
-
-  var newButton = $("#animalInput").val().trim();
-  
-  animals.push(newButton);
-
-  $("animalInput").empty();
-  renderButtons();
-});
-
-renderButtons();
+            $("#buttonsHolder").append(newButton);
 
 
+        }
+    }
+    //adds new buttons to the page
+    $("body").on("click", "#submitAnimal", function (event) {
+        event.preventDefault();
 
-// On button click AJAX function is performed to connect to Giphy API. 
-$("button").on("click", function() {
+        var newButton = $("#animalInput").val().trim();
 
-    var animalName = $(this).attr("data-animal");
+        animals.push(newButton);
 
-   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=oJBlmF9dfs5DDgoNqZfhbZUiUcqCWqDv&limit=10";
+        $("animalInput").empty();
+        renderButtons();
+    });
+
+    renderButtons();
 
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
-    console.log(queryURL);
-    
-    // store response data within results variable
-    var results = response.data;
-    // loop through results creating and displaying the content
-    for (let i = 0; i < results.length; i++) {
-        
-        var rating = results[i].rating;
-        
-        var p = $("<p>").text("Rating: " + rating);
 
-        var giphyImage = $("<img>");
-        giphyImage.attr("src", results[i].images.fixed_height.url);
-        giphyImage.addClass("gifs");
-        giphyImage.attr("still", results[i].images.fixed_height_still.url);
-        giphyImage.attr("animated", results[i].images.fixed_height.url);
-        giphyImage.attr("state", "animated");
-        $("#animalsHolder").prepend(p);
-        $("#animalsHolder").prepend(giphyImage);
+    // On button click AJAX function is performed to connect to Giphy API. 
+    function getGifs() {
+
+        var animalName = $(this).attr("data-animal");
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=oJBlmF9dfs5DDgoNqZfhbZUiUcqCWqDv&limit=10";
+
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            console.log(queryURL);
+
+            // store response data within results variable
+            var results = response.data;
+            // loop through results creating and displaying the content
+            for (let i = 0; i < results.length; i++) {
+
+                var rating = results[i].rating;
+
+                var p = $("<p>").text("Rating: " + rating);
+
+                var giphyImage = $("<img>");
+                giphyImage.attr("src", results[i].images.fixed_height.url);
+                giphyImage.addClass("gifs");
+                giphyImage.attr("still", results[i].images.fixed_height_still.url);
+                giphyImage.attr("animated", results[i].images.fixed_height.url);
+                giphyImage.attr("state", "animated");
+                $("#animalsHolder").prepend(p);
+                $("#animalsHolder").prepend(giphyImage);
+            }
+
+        });
     }
 
-});
-});
+    // Plays or pauses gifs on user click
 
-// Plays or pauses gifs on user click
+    $("body").on("click", ".gifs", function () {
 
-$("body").on("click", ".gifs", function(){
-    
-    var state = $(this).attr("state");
+        var state = $(this).attr("state");
 
-    if(state === "animated"){
-        $(this).attr("src", $(this).attr("still"));
-        $(this).attr("state", "still");
-    }
-    if(state === "still"){
-        $(this).attr("src", $(this).attr("animated"));
-        $(this).attr("state", "animated");
-    };
-});
+        if (state === "animated") {
+            $(this).attr("src", $(this).attr("still"));
+            $(this).attr("state", "still");
+        }
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("animated"));
+            $(this).attr("state", "animated");
+        };
+    });
 });
